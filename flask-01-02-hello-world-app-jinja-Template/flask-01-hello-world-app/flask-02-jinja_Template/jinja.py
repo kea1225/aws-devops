f1e95d6a54fd61197ec2494689c7c69ffbfa5249
karil <msc.int_llc@yahoo.com>
2021-10-05 22:29:16 -0400

    name change

--- flask-01-02-hello-world-app-jinja-Template/flask-01-hello-world-app/flask-02-jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-01-hello-world-app/flask-02-jinja_Template/jinja.py
@@ -5,16 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def head():
     return render_template("index.html", 
-    numero1=12, numero2=15)
+    numeber1=12, number2=15)
 
 @app.route("/karil")
 def calculer():
     num1, num2 = 23,45
-    return render_template(kaflask.html, value1=num1, value2=num2, 
+    return render_template(calcule.html, value1=num1, value2=num2, 
     value3=num1*num2)
 
 if __name__=="__main__":
-    app.run(host='0000',port=80)
-   # app.run(debug=True)
+    #app.run(host='0000',port=80)
+    app.run(debug=True)
     
-   
